d49678205f9cb15995e11cff841bbf23bb689b92
walteribanez555 <walteribanez555@gmail.com>
2024-04-21 21:04:12 -0400

    added assets fonts Lato

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
-import { AppComponent } from './app/app.component';
+import { appConfig } from './app/presentation/app.config';
+import { AppComponent } from './app/presentation/app.component';
 
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
